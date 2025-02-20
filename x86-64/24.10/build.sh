c80a1229c02eacfed41230c3f26d1d5b4a0c5fe1
Lucifer <690699341@qq.com>
2025-02-20 14:00:52 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -20,7 +20,7 @@ cat /home/build/immortalwrt/files/etc/config/pppoe-settings
 # 输出调试信息
 echo "$(date '+%Y-%m-%d %H:%M:%S') - 开始编译..."
 
-
+echo $(ls `$PACKAGES`)
 
 # 定义所需安装的包列表 下列插件你都可以自行删减
 PACKAGES=""
