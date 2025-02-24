12b6cf8a7fdbd914e2a54e5890a6a128e68e04e1
Lucifer <690699341@qq.com>
2025-02-24 11:18:13 +0800

    Update build.sh

--- x86-64/24.10/build.sh
+++ x86-64/24.10/build.sh
@@ -44,9 +44,11 @@ PACKAGES="$PACKAGES script-utils"
 PACKAGES="$PACKAGES luci-i18n-samba4-zh-cn"
 
 # 增加自定义软件
-#PACKAGES="$PACKAGES nikki"
-#PACKAGES="$PACKAGES luci-app-nikki"
-#PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
+PACKAGES="$PACKAGES nikki"
+PACKAGES="$PACKAGES luci-app-nikki"
+PACKAGES="$PACKAGES luci-i18n-nikki-zh-cn"
+
+git clone https://github.com/ffuqiangg/openwrt_apps.git ./package/new/luci-app-nikki
 
 # 判断是否需要编译 Docker 插件
 if [ "$INCLUDE_DOCKER" = "yes" ]; then
